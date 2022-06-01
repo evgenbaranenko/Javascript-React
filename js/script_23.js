48425d0b3a48c175922c6929180931b3e4b43dbb
evgen <evgenbaranenko@gmail.com>
2022-06-01 17:11:33 +0300

    lesson_24_homework

--- js/script_23.js
+++ js/script_23.js
@@ -4,11 +4,21 @@ let result = "";
 let result2 = "";
 const lenght = 10;
 
-for (let i = 0; i < lenght; i++) {
+for (let i = 0; i < lenght; ) {
+    i++;
     for (let j = 0; j < i; j++) {
         result += "*";
     }
     result += '\n';
 }
-console.log(result);
 
+console.log(result);
+//console.log("\n");
+
+for (let x = 0; x < lenght; x++) {
+    for (let j = lenght; j > x; j--) {
+        result2 += "*";
+    }
+    result2 += '\n';
+}
+console.log(result2);
